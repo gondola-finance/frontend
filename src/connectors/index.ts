eec9f9fffc34e9377b84622ebffddfa695bebdd3
whisidev <dev@gondola.finance>
2021-03-22 19:25:42 +0800

    update chain id constant

--- src/connectors/index.ts
+++ src/connectors/index.ts
@@ -1,13 +1,12 @@
+import { ChainId } from "../constants"
 import { InjectedConnector } from "@web3-react/injected-connector"
 import { NetworkConnector } from "@web3-react/network-connector"
-import { WalletConnectConnector } from "@web3-react/walletconnect-connector"
-import { WalletLinkConnector } from "@web3-react/walletlink-connector"
 import { Web3Provider } from "@ethersproject/providers"
 
 const NETWORK_URL = process.env.REACT_APP_NETWORK_URL
-export const NETWORK_CHAIN_ID: number = parseInt(
-  process.env.REACT_APP_CHAIN_ID ?? "1",
-)
+export const NETWORK_CHAIN_ID: number = process.env.REACT_APP_CHAIN_ID
+  ? parseInt(process.env.REACT_APP_CHAIN_ID)
+  : ChainId.AVALANCHE
 
 if (typeof NETWORK_URL === "undefined") {
   throw new Error(
@@ -29,19 +28,6 @@ export function getNetworkLibrary(): Web3Provider {
 }
 
 export const injected = new InjectedConnector({
-  // mainnet, ropsten, rinkeby, goerli, kovan, local buidler
-  // see: https://github.com/ethereum/EIPs/blob/master/EIPS/eip-155.md
-  supportedChainIds: [43113, 43114],
-})
-
-export const walletconnect = new WalletConnectConnector({
-  rpc: { [NETWORK_CHAIN_ID]: NETWORK_URL },
-  bridge: "https://bridge.walletconnect.org",
-  qrcode: true,
-  // pollingInterval: POLLING_INTERVAL / 12000
-})
-
-export const walletlink = new WalletLinkConnector({
-  url: NETWORK_URL,
-  appName: "Gondola",
+  // mainnet, fuji
+  supportedChainIds: [ChainId.AVALANCHE, ChainId.FUJI],
 })
