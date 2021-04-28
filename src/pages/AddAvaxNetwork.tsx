92b1bbc5305dcf5589d19e7e55f12cda2f36bef1
whisidev <dev@gondola.finance>
2021-04-28 17:40:00 +0800

    add page for switching to avax chain

---
+++ src/pages/AddAvaxNetwork.tsx
@@ -0,0 +1,65 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+import "./AddAvaxNetwork.scss"
+
+import { Button, Center } from "@chakra-ui/react"
+import React, { ReactElement } from "react"
+import Footer from "../components/Footer"
+import TopMenu from "../components/TopMenu"
+import { injected } from "../connectors"
+
+export const AVALANCHE_MAINNET_PARAMS = {
+  chainId: "0xa86a", // A 0x-prefixed hexadecimal chainId
+  chainName: "Avalanche Mainnet C-Chain",
+  nativeCurrency: {
+    name: "Avalanche",
+    symbol: "AVAX",
+    decimals: 18,
+  },
+  rpcUrls: ["https://api.avax.network/ext/bc/C/rpc"],
+  blockExplorerUrls: ["https://cchain.explorer.avax.network/"],
+}
+
+function addAvalancheNetwork() {
+  void injected.getProvider().then((provider) => {
+    if (!provider) return
+    provider
+      .request({
+        method: "wallet_addEthereumChain",
+        params: [AVALANCHE_MAINNET_PARAMS],
+      })
+      .catch((error: unknown) => {
+        console.error(error)
+      })
+  })
+}
+
+function AddAvaxNetwork(): ReactElement {
+  return (
+    <div className="avaxNetPage">
+      <TopMenu activeTab={"swap"} />
+      <div className="content">
+        <Center width="100%" py={6}>
+          <div>
+            To use Gondola Dapp, please connect to the Avalanche network.
+          </div>
+        </Center>
+
+        <Center width="100%" py={6}>
+          <Button
+            variant="primary"
+            size="lg"
+            onClick={(): void => {
+              addAvalancheNetwork()
+            }}
+          >
+            Switch to Avalanche chain
+          </Button>
+        </Center>
+        <Footer />
+      </div>
+    </div>
+  )
+}
+
+export default AddAvaxNetwork
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -2,6 +2,7 @@ import "../styles/global.scss"
 
 import {
   BLOCK_TIME,
+  ChainId,
   ZDAI_DAI_POOL_NAME,
   ZETH_ETH_POOL_NAME,
   ZUSDT_USDT_POOL_NAME,
@@ -9,6 +10,7 @@ import {
 import React, { ReactElement, Suspense, useCallback } from "react"
 import { Route, Switch } from "react-router-dom"
 
+import AddAvaxNetwork from "./AddAvaxNetwork"
 import { AppDispatch } from "../state"
 import Deposit from "./Deposit"
 import Pools from "./Pools"
@@ -20,6 +22,7 @@ import Web3ReactManager from "../components/Web3ReactManager"
 import Withdraw from "./Withdraw"
 import fetchGasPrices from "../utils/updateGasPrices"
 import fetchTokenPricesUSD from "../utils/updateTokenPrices"
+import { useActiveWeb3React } from "../hooks"
 import { useDispatch } from "react-redux"
 import usePoller from "../hooks/usePoller"
 
@@ -33,93 +36,106 @@ export default function App(): ReactElement {
   }, [dispatch])
   usePoller(fetchAndUpdateGasPrice, BLOCK_TIME)
   usePoller(fetchAndUpdateTokensPrice, BLOCK_TIME * 3)
+  const { chainId } = useActiveWeb3React()
+
+  let page
+
+  if (!chainId) {
+    console.log("not connected. please install metamask :(")
+  }
+  if (chainId && chainId !== ChainId["AVALANCHE"]) {
+    // wrong network
+    page = <AddAvaxNetwork />
+  }
 
   return (
     <Suspense fallback={null}>
       <Web3ReactManager>
         <ToastsProvider>
-          <Switch>
-            <Route
-              exact
-              path="/"
-              render={(props) => <Pools action="swap" {...props} />}
-            />
-            <Route
-              exact
-              path="/swap/dai"
-              render={(props) => (
-                <Swap {...props} poolName={ZDAI_DAI_POOL_NAME} />
-              )}
-            />
-            <Route
-              exact
-              path="/swap/eth"
-              render={(props) => (
-                <Swap {...props} poolName={ZETH_ETH_POOL_NAME} />
-              )}
-            />
-            <Route
-              exact
-              path="/swap/usdt"
-              render={(props) => (
-                <Swap {...props} poolName={ZUSDT_USDT_POOL_NAME} />
-              )}
-            />
-            <Route
-              exact
-              path="/deposit"
-              render={(props) => <Pools action="deposit" {...props} />}
-            />
-            <Route
-              exact
-              path="/deposit/eth"
-              render={(props) => (
-                <Deposit {...props} poolName={ZETH_ETH_POOL_NAME} />
-              )}
-            />
-            <Route
-              exact
-              path="/deposit/dai"
-              render={(props) => (
-                <Deposit {...props} poolName={ZDAI_DAI_POOL_NAME} />
-              )}
-            />
-            <Route
-              exact
-              path="/deposit/usdt"
-              render={(props) => (
-                <Deposit {...props} poolName={ZUSDT_USDT_POOL_NAME} />
-              )}
-            />
-            <Route
-              exact
-              path="/withdraw"
-              render={(props) => <Pools action="withdraw" {...props} />}
-            />
-            <Route
-              exact
-              path="/withdraw/dai"
-              render={(props) => (
-                <Withdraw {...props} poolName={ZDAI_DAI_POOL_NAME} />
-              )}
-            />
-            <Route
-              exact
-              path="/withdraw/eth"
-              render={(props) => (
-                <Withdraw {...props} poolName={ZETH_ETH_POOL_NAME} />
-              )}
-            />
-            <Route
-              exact
-              path="/withdraw/usdt"
-              render={(props) => (
-                <Withdraw {...props} poolName={ZUSDT_USDT_POOL_NAME} />
-              )}
-            />
-            <Route exact path="/stake" render={() => <Stake />} />
-            <Route exact path="/risk" component={Risk} />
-          </Switch>
+          {page || (
+            <Switch>
+              <Route
+                exact
+                path="/"
+                render={(props) => <Pools action="swap" {...props} />}
+              />
+              <Route
+                exact
+                path="/swap/dai"
+                render={(props) => (
+                  <Swap {...props} poolName={ZDAI_DAI_POOL_NAME} />
+                )}
+              />
+              <Route
+                exact
+                path="/swap/eth"
+                render={(props) => (
+                  <Swap {...props} poolName={ZETH_ETH_POOL_NAME} />
+                )}
+              />
+              <Route
+                exact
+                path="/swap/usdt"
+                render={(props) => (
+                  <Swap {...props} poolName={ZUSDT_USDT_POOL_NAME} />
+                )}
+              />
+              <Route
+                exact
+                path="/deposit"
+                render={(props) => <Pools action="deposit" {...props} />}
+              />
+              <Route
+                exact
+                path="/deposit/eth"
+                render={(props) => (
+                  <Deposit {...props} poolName={ZETH_ETH_POOL_NAME} />
+                )}
+              />
+              <Route
+                exact
+                path="/deposit/dai"
+                render={(props) => (
+                  <Deposit {...props} poolName={ZDAI_DAI_POOL_NAME} />
+                )}
+              />
+              <Route
+                exact
+                path="/deposit/usdt"
+                render={(props) => (
+                  <Deposit {...props} poolName={ZUSDT_USDT_POOL_NAME} />
+                )}
+              />
+              <Route
+                exact
+                path="/withdraw"
+                render={(props) => <Pools action="withdraw" {...props} />}
+              />
+              <Route
+                exact
+                path="/withdraw/dai"
+                render={(props) => (
+                  <Withdraw {...props} poolName={ZDAI_DAI_POOL_NAME} />
+                )}
+              />
+              <Route
+                exact
+                path="/withdraw/eth"
+                render={(props) => (
+                  <Withdraw {...props} poolName={ZETH_ETH_POOL_NAME} />
+                )}
+              />
+              <Route
+                exact
+                path="/withdraw/usdt"
+                render={(props) => (
+                  <Withdraw {...props} poolName={ZUSDT_USDT_POOL_NAME} />
+                )}
+              />
+              <Route exact path="/stake" render={() => <Stake />} />
+              <Route exact path="/risk" component={Risk} />
+            </Switch>
+          )}
         </ToastsProvider>
       </Web3ReactManager>
     </Suspense>
